27e19d3e48e6aae4883a64dbc4d361f08e58f576
Gerald Bustos Morales <geraldbustosm@gmail.com>
2020-05-04 23:49:49 -0400

    add comments

--- script.py
+++ script.py
@@ -2,14 +2,19 @@ import requests
 import json
 import datetime
 
+# Get time to fetch current csv
 date = datetime.datetime.now()
 
 res = requests.get('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/' + date.strftime("%Y-%m-%d") + '-CasosConfirmados-totalRegional.csv')
 csv = res.text
+
+# Initial variables
 text = ""
 data = []
 subdata = []
 i = 0
+
+# Convert csv into array
 while(i < len(csv)):
     if(i < len(csv) - 1 and csv[i] == ' ' and csv[i+1] == ' '):
         i = i + 1
